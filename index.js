ad570bfda9dd736fce62094d15fffca3e3853ba8
Prasoon Soni <prasoonsoni03@gmail.com>
2022-10-31 22:40:03 +0530

    Added Middleware to getUser ID

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const app = express()
 const cors = require('cors')
 const port = 3000 || process.env.PORT
 const connectToDatabase = require('./database/connection')
+const getUser = require('./middleware/getUser')
 app.use(cors())
 app.use(express.json())
+app.use(getUser)
 connectToDatabase()
 
 app.get('/', (req, res) => {
